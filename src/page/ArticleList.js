24a5d8c5260ec1523523a56f8a73635dc5c809e1
foreverfirst <2279275691@qq.com>
2020-08-31 19:18:33 +0800

    博客列表

--- src/page/ArticleList.js
+++ src/page/ArticleList.js
@@ -1,36 +1,72 @@
 import React from 'react'
 
-import { Upload, message, Button } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import {Table, Radio, Input, Button} from 'antd';
+import {SearchOutlined} from '@ant-design/icons';
 
-const props = {
-  name: 'file',
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  headers: {
-    authorization: 'authorization-text',
-  },
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
+const dataSource = [
+    {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+];
+
+const columns = [
+    {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+
 
 
 export default class BlogDetail extends React.Component {
 
-  render(){
-      return (
-        <Upload {...props}>
-        <Button>
-          <UploadOutlined /> Click to Upload
-        </Button>
-      </Upload>
-      )    
+  handleLink = (e) => {
+
   }
+
+    render() {
+        return (
+            <div>
+        <Radio.Group defaultValue="a" buttonStyle="solid" onChange={this.handleLink } style={{marginTop: 16}}>
+          <Radio.Button value="a" >全部文章</Radio.Button>
+          <Radio.Button value="b" disabled>已发表</Radio.Button>
+          <Radio.Button value="c">草稿箱</Radio.Button>
+          <Radio.Button value="d">回收站</Radio.Button>
+          <Radio.Button value="e">博客管理</Radio.Button>
+          <Radio.Button value="f">博客配置</Radio.Button>
+        </Radio.Group>
+        <div style={{marginTop:15}}>
+           <Input placeholder={"通过标题搜索该分类下的博客..."} style={{width: 400}} size={"middle"} >
+
+        </Input>
+          <Button icon={<SearchOutlined />} style={{marginLeft:5}} size={"middle"}>
+          </Button>
+        </div>
+              <Table dataSource={dataSource} columns={columns}/>
+
+
+
+            </div>
+    )
+    }
 }
